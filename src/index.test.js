4984980d7d850cfda033b599c48d933545c31d60
Osman Nuri Okumuş <onokumus@gmail.com>
2016-06-18 01:07:24 +0300

    ilk version yayınlandı

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,27 @@
 var expect = require('chai').expect;
 var ilAd = require('./index');
 
-describe('ilAd', function () {
-  it('should work!', function(){
-    expect(true).to.be.true;
-  });
+describe('ilAd', function() {
+    describe('all', function() {
+        it('should be an array of strings', function() {
+            expect(ilAd.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+
+        it('should contain `ankara`', function() {
+            expect(ilAd.all).to.include('ankara');
+        });
+    });
+
+    describe('random', function() {
+        it('should return a random item from ilAd.all', function() {
+            var randomItem = ilAd.random();
+            expect(ilAd.all).to.include(randomItem);
+        });
+    });
 });
